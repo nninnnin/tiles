87b3453b2772ff097c4a125c5f63a556653fa1e6
donggyu <watch@rebel9.co.kr>
2024-06-11 10:47:48 +0900

    람다 클라이언트 세팅 (with Webpack)

---
+++ lambda-client.js
@@ -0,0 +1,5 @@
+const {
+  LambdaClient,
+} = require("@aws-sdk/client-lambda");
+
+console.log('This is lambda client!', LambdaClient)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.html',
-  output: path.resolve(__dirname, 'dist')
+  entry: './lambda-client.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'lambda-client-bundled.js'
+  }
 }
